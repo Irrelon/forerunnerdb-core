72d9a961cb6f992083c8553bdb0effdf5a373bca
robevans <rob@irrelon.com>
2020-11-29 12:57:39 +0000

    Added update() to import test

--- src/main.test.js
+++ src/main.test.js
@@ -1,8 +1,12 @@
-import {find} from "./main.js";
+import {find, update} from "./main.js";
 import * as assert from "assert";
 
 describe("main", () => {
 	it("Imported find() correctly", () => {
 		assert.strictEqual(find instanceof Function, true, "Is a function");
+	});
+
+	it("Imported update() correctly", () => {
+		assert.strictEqual(update instanceof Function, true, "Is a function");
 	});
 });
