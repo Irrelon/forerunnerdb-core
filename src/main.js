4a08849d0ddd40af4c18c1fefc8a1632ecf0c375
robevans <rob@irrelon.com>
2020-11-29 12:59:00 +0000

    More imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Collection from "./core/Collection";
 export {Collection};
 
+export {insert} from "./core/operation/insert";
 export {find} from "./core/operation/find";
 export {update} from "./core/operation/update";
+export {remove} from "./core/operation/remove";
 export {queryToPipeline} from "./core/operation/build";
--- src/main.test.js
+++ src/main.test.js
@@ -1,12 +1,20 @@
-import {find, update} from "./main.js";
+import {insert, find, update, remove} from "./main.js";
 import * as assert from "assert";
 
 describe("main", () => {
+	it("Imported insert() correctly", () => {
+		assert.strictEqual(insert instanceof Function, true, "Is a function");
+	});
+
 	it("Imported find() correctly", () => {
 		assert.strictEqual(find instanceof Function, true, "Is a function");
 	});
 
 	it("Imported update() correctly", () => {
 		assert.strictEqual(update instanceof Function, true, "Is a function");
+	});
+
+	it("Imported remove() correctly", () => {
+		assert.strictEqual(remove instanceof Function, true, "Is a function");
 	});
 });
